7c48b73460baff697292a02d885218372c949525
yanislavvejdarski <yanislav.vejdarski@gmail.com>
2020-04-17 18:23:37 +0300

    bug fixes

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value;
     fetch('/weather?address=' + location).then((response) => {
+        messageOne.textContent = 'Loading...'
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,10 +2,7 @@ const request = require('request');
 require('dotenv').config();
 const accessToken = process.env.ACCESS_TOKEN
 const geocode = (address, callback) => {
-    const url =
-        'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-        encodeURIComponent(address) +
-        '.json?access_token='+accessToken;
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token='+accessToken+'&limit=1';
     request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined);
